c7612aad36f8f6ed2ba17933360123e895291627
CHENG-Chvo <108559990+CHENG-Chvo@users.noreply.github.com>
2023-07-09 15:29:28 +0800

    Update Index.php

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -1,109 +1,29 @@
 <?php
-// +----------------------------------------------------------------------
-// | 文件: index.php
-// +----------------------------------------------------------------------
-// | 功能: 提供todo api接口
-// +----------------------------------------------------------------------
-// | 时间: 2021-11-15 16:20
-// +----------------------------------------------------------------------
-// | 作者: rangangwei<gangweiran@tencent.com>
-// +----------------------------------------------------------------------
+namespace app\index\controller;
 
-namespace app\controller;
+use think\Controller;
+use think\Request;
+use think\Http;
 
-use Error;
-use Exception;
-use app\model\Counters;
-use think\response\Html;
-use think\response\Json;
-use think\facade\Log;
-
-class Index
+class WeatherController extends Controller
 {
-
-    /**
-     * 主页静态页面
-     * @return Html
-     */
-    public function index(): Html
+    public function index(Request $request)
     {
-        # html路径: ../view/index.html
-        return response(file_get_contents(dirname(dirname(__FILE__)).'/view/index.html'));
-    }
+        $location = $request->param('location'); // 获取用户发送的地点
 
+        $api_key = "PeKpZPeGMdYI-Cdno"; // 在心知天气官网注册并获取API Key
+        $api_url = "https://api.seniverse.com/v3/weather/now.json?key={$api_key}&location={$location}&language=zh-Hans&unit=c";
 
-    /**
-     * 获取todo list
-     * @return Json
-     */
-    public function getCount(): Json
-    {
-        try {
-            $data = (new Counters)->find(1);
-            if ($data == null) {
-                $count = 0;
-            }else {
-                $count = $data["count"];
-            }
-            $res = [
-                "code" => 0,
-                "data" =>  $count
-            ];
-            Log::write('getCount rsp: '.json_encode($res));
-            return json($res);
-        } catch (Error $e) {
-            $res = [
-                "code" => -1,
-                "data" => [],
-                "errorMsg" => ("查询计数异常" . $e->getMessage())
-            ];
-            Log::write('getCount rsp: '.json_encode($res));
-            return json($res);
-        }
-    }
+        $response = Http::get($api_url);
+        $data = json_decode($response->getBody(), true);
 
-
-    /**
-     * 根据id查询todo数据
-     * @param $action `string` 类型，枚举值，等于 `"inc"` 时，表示计数加一；等于 `"reset"` 时，表示计数重置（清零）
-     * @return Json
-     */
-    public function updateCount($action): Json
-    {
-        try {
-            if ($action == "inc") {
-                $data = (new Counters)->find(1);
-                if ($data == null) {
-                    $count = 1;
-                }else {
-                    $count = $data["count"] + 1;
-                }
-    
-                $counters = new Counters;
-                $counters->create(
-                    ["count" => $count, 'id' => 1],
-                    ["count", 'id'],
-                    true
-                );
-            }else if ($action == "clear") {
-                Counters::destroy(1);
-                $count = 0;
-            }
-
-            $res = [
-                "code" => 0,
-                "data" =>  $count
-            ];
-            Log::write('updateCount rsp: '.json_encode($res));
-            return json($res);
-        } catch (Exception $e) {
-            $res = [
-                "code" => -1,
-                "data" => [],
-                "errorMsg" => ("更新计数异常" . $e->getMessage())
-            ];
-            Log::write('updateCount rsp: '.json_encode($res));
-            return json($res);
+        // 解析天气信息
+        if (isset($data["results"])) {
+            $weather = $data["results"][0]["now"]["text"];
+            $temperature = $data["results"][0]["now"]["temperature"];
+            return "地点：{$location}\n天气：{$weather}\n温度：{$temperature}℃";
+        } else {
+            return "无法获取天气信息，请检查地点是否正确。";
         }
     }
 }
